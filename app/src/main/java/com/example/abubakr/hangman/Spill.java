f27b3c819d4617637e6cf1cdf7f1547bd1127c0d
Abubakr <abumiqb@gmail.com>
2017-09-04 21:51:27 +0200

    Working array and randomgenerator

--- app/src/main/java/com/example/abubakr/hangman/Spill.java
+++ app/src/main/java/com/example/abubakr/hangman/Spill.java
@@ -5,13 +5,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import java.util.Random;
+
 public class Spill extends AppCompatActivity
 {
-
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_spill);
+        String[] array = getResources().getStringArray(R.array.OrdListe); //Henter array med ord fra arrays.xml
+        String randomStr = array[new Random().nextInt(array.length)]; //Velger random ord fra arrayet
+        TextView textView = (TextView)findViewById(R.id.txt); // Oppretter textview
+        textView.setText(randomStr);
     }
 }
