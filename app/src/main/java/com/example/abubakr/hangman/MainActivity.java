6decbeb92e787d20e55c68fceed3922dacbc90cf
Abubakr <abumiqb@gmail.com>
2017-09-15 20:46:36 +0200

    Hangman virker 1.3

--- app/src/main/java/com/example/abubakr/hangman/MainActivity.java
+++ app/src/main/java/com/example/abubakr/hangman/MainActivity.java
@@ -14,6 +14,8 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
     }
 
+    // The World Is Yours
+
     public void Spill (View view)
     {
         Intent intent = new Intent (this, Spill.class);
